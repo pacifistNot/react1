0a0b5761af5a2b492cecc04ac084cc3fbdafdd2f
pacifistNot <s.sumenkov560@mail.ru>
2023-04-14 16:44:53 +0400

    +

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import StudListing from './StudListing';
-import StudCreate from './StudCreate';
 import StudDetails from './StudDetails';
-import StudEdit from './StudEdit';
 
 function App() {
   return (
@@ -12,10 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<StudListing />}></Route>
-          {/* <Route path='/student/create' element={<StudCreate />}></Route> */}
-
           <Route path='/student/detail/:studid' element={<StudDetails />}></Route>
-          {/* <Route path='/student/edit/:studid' element={<StudEdit />}></Route> */}
         </Routes>
       </BrowserRouter>
     </div>
--- src/StudListing.js
+++ src/StudListing.js
@@ -1,25 +1,32 @@
-
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 const StudListing = () => {
     const [studdata, studdatachange] = useState([]);
-    
+
     const navigate = useNavigate();
-    const { studid } = useParams();
+
     const [id, idchange] = useState("");
     const [name, namechange] = useState("");
     const [surname, surnamechange] = useState("");
     const [date, datechange] = useState("");
     const [group, groupchange] = useState("");
     const [point, pointchange] = useState("");
-    const [validation, valchange] = useState(false);
 
+    useEffect(() => {
+        fetch("http://localhost:8000/student").then((res) => {
+            return res.json();
+        }).then((resp) => {
+            studdatachange(resp);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }, [])
+    
 
     // ПОКАЗАТЬ ЕЩЕ
     const [noOfElement, setnoOfElement] = useState(10);
-    const LoadMore = () =>{
+    const LoadMore = () => {
         setnoOfElement(noOfElement + noOfElement);
     }
     const slice = studdata.slice(0, noOfElement);
@@ -30,6 +37,7 @@ const StudListing = () => {
         navigate("/student/detail/" + id);
     }
 
+
     // ФУНКЦИЯ УДАЛЕНИЯ
     const Removefunction = (id) => {
         fetch("http://localhost:8000/student/" + id, {
@@ -39,18 +47,10 @@ const StudListing = () => {
         })
     }
 
-    // ФУНКЦИЯ ДОБАВЛЕНИЯ
-    useEffect(() => {
-        fetch("http://localhost:8000/student").then((res) => {
-            return res.json();
-        }).then((resp) => {
-            studdatachange(resp);
-        }).catch((err) => {
-            console.log(err.message);
-        })
-    }, [])
 
-    const handleAdd = (e) => {
+    // ФУНКЦИЯ ДОБАВЛЕНИЯ
+    const handleAdd = (e) => {      
+
         e.preventDefault();
         const studdata = { name, surname, date, group, point };
         fetch("http://localhost:8000/student/", {
@@ -65,12 +65,12 @@ const StudListing = () => {
     }
 
 
-    // ФУНКЦИЯ РЕДАКТИРОВАНИЯ (НЕ РАБОТАЕТ)
+    // ФУНКЦИЯ РЕДАКТИРОВАНИЯ
     const handlesubmit = (e) => {
         e.preventDefault();
         const studdata = { id, name, surname, date, group, point };
 
-        fetch("http://localhost:8000/student" + studid, {
+        fetch("http://localhost:8000/student/" + id, {
             method: "PUT",
             headers: { "content-type": "application/json" },
             body: JSON.stringify(studdata)
@@ -81,22 +81,15 @@ const StudListing = () => {
         })
     }
 
-    // useEffect(() => {
-    //     fetch("http://localhost:8000/student" + studid).then((res) => {
-    //         return res.json();
-    //     }).then((resp) => {
-    //         idchange(resp.id);
-    //         namechange(resp.name);
-    //         surnamechange(resp.surname);
-    //         datechange(resp.date);
-    //         groupchange(resp.group);
-    //         pointchange(resp.point);
-    //     }).catch((err) => {
-    //         console.log(err.message);
-    //     })
-    // }, []);
-
-    
+    function func(data) {
+        const { id, name, surname, date, group, point } = data
+        idchange(id)
+        namechange(name)
+        surnamechange(surname)
+        datechange(date)
+        groupchange(group)
+        pointchange(point)
+    }
 
     return (
         <div className="container">
@@ -107,13 +100,13 @@ const StudListing = () => {
                 <div className="card-body">
                     <div className="card__body__up">
                         <button type="button" className="btn btn-success btn__add__stud" data-toggle="modal" data-target="#add">
-                            Добавить студента
+                            Добавить студента (+)
                         </button>
                     </div>
                     <table className="table table-border">
                         <thead className="bg-dark text-white">
                             <tr>
-                                <td>ID</td>
+                                <td className="list__item__id">ID</td>
                                 <td>Имя</td>
                                 <td>Фамилия</td>
                                 <td>Дата рождения</td>
@@ -125,16 +118,16 @@ const StudListing = () => {
                         <tbody>
 
                             {slice &&
-                                slice.map((item, index) => (
-                                    <tr key={index}>
-                                        <td>{item.id}</td>
+                                slice.map((item, id) => (
+                                    <tr key={id}>
+                                        <td className="list__item__id">{item.id}</td>
                                         <td><a className="Detail__link" onClick={() => { StudDetail(item.id) }}>{item.name}</a></td>
                                         <td><a className="Detail__link" onClick={() => { StudDetail(item.id) }}>{item.surname}</a></td>
                                         <td>{item.date}</td>
                                         <td>{item.group}</td>
                                         <td>{item.point}</td>
                                         <td>
-                                            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Редактировать</button>
+                                            <button type="button" className="btn btn-primary" onClick={() => func(item)} data-toggle="modal" data-target="#exampleModal">Редактировать</button>
                                             <button className="btn btn-danger" onClick={() => { Removefunction(item.id) }}>Удалить</button>
                                         </td>
                                     </tr>
@@ -148,11 +141,11 @@ const StudListing = () => {
                         <div className="modal-dialog">
                             <div className="modal-content">
                                 <div className="modal-header">
-                                    <h3 className="modal-title" id="exampleModalLabel">Введите данные студента</h3>
+                                    <h3 className="modal-title" id="exampleModalLabel">Добавить студента</h3>
                                 </div>
                                 <div className="modal-body">
                                     <form onSubmit={handleAdd}>
-                                        <div className="form-group">
+                                        <div className="form-group list__item__id">
                                             <label>ID</label>
                                             <input value={id} disabled="disabled" className="form-control"></input>
                                         </div>
@@ -191,17 +184,17 @@ const StudListing = () => {
                         <div className="modal-dialog">
                             <div className="modal-content">
                                 <div className="modal-header">
-                                    <h3 className="modal-title" id="exampleModalLabel">Редактор данных студента</h3>
+                                    <h3 className="modal-title" id="exampleModalLabel">Редактировать данные студента</h3>
                                 </div>
                                 <div className="modal-body">
-                                    <form className="container" onSubmit={handlesubmit}>
-                                        <div className="form-group">
+                                    <form onSubmit={handlesubmit}>
+                                        <div className="form-group list__item__id">
                                             <label>ID</label>
                                             <input value={id} disabled="disabled" className="form-control"></input>
                                         </div>
                                         <div className="form-group">
                                             <label>Имя</label>
-                                            <input required type="text" value={name} onMouseDown={e => valchange(true)} onChange={e => namechange(e.target.value)} className="form-control"></input>
+                                            <input required type="text" value={name}  onChange={e => namechange(e.target.value)} className="form-control"></input>
                                         </div>
                                         <div className="form-group">
                                             <label>Фамилия</label>
